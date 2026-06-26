d7dc2711405914e0ea1048f61915bdac7f2af521
Foxymilyan <Foxymilyan.Fox@gmail.com>
2026-06-26 15:27:15 +0300

    First Commit

---
+++ addons/main/config.cpp
@@ -0,0 +1,18 @@
+class CfgFunctions
+{
+    class FOX
+    {
+        class uncon_spectator
+        {
+            file = "\f\fox\addons\main\functions";
+
+            class init
+            {
+                postInit = 1;
+            };
+
+            class openSpectator {};
+            class closeSpectator {};
+        };
+    };
+};
